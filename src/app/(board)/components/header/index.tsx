b3a58245810b1b91be5527d8dd709f6d089d486c
Bruno Duarte <bruno.duarte314@gmail.com>
2026-02-25 16:40:45 -0300

    refactor: update user-button component location

--- src/app/(board)/components/header/index.tsx
+++ src/app/(board)/components/header/index.tsx
@@ -1,5 +1,5 @@
-import { SearchInput } from "./search-input";
-import { UserButton } from "./user-button";
+import { SearchInput } from "@/app/(board)/components/header/search-input";
+import { UserButton } from "@/components/header/user-button";
 
 export function Header() {
   return (
