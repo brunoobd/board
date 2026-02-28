ee76f360ba1d5f6abb104c142804ed6b34861c83
Bruno Duarte <bruno.duarte314@gmail.com>
2026-02-28 13:08:38 -0300

    feat: add like-button funcionality

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from "react";
 import Link from "next/link";
 import type { Metadata } from "next";
-import { MoveLeftIcon, ArchiveIcon, ThumbsUpIcon } from "lucide-react";
+import { MoveLeftIcon, ArchiveIcon, MessageCirclePlusIcon } from "lucide-react";
 import { getIssue } from "@/http/get-issue";
-import { Button } from "@/components/button";
 import { IssueCommentList } from "@/app/issues/_components/comment/list";
 import { IssueCommentListSkeleton } from "@/app/issues/_components/comment/list/skeleton";
+import { Input } from "@/components/input";
+import { IssueLikeButton } from "@/app/issues/_components/like-button";
 
 interface IssuePageProps {
   params: Promise<{ id: string }>;
@@ -51,10 +52,7 @@ export default async function IssuePage({ params }: IssuePageProps) {
           {statusLabels[issue.status]}
         </span>
 
-        <Button>
-          <ThumbsUpIcon className="size-3" />
-          <span className="text-sm">12</span>
-        </Button>
+        <IssueLikeButton issueId={issue.id} />
       </div>
 
       <div className="space-y-2">
@@ -67,7 +65,19 @@ export default async function IssuePage({ params }: IssuePageProps) {
       <div className="flex flex-col gap-2">
         <span className="font-semibold">Comments</span>
 
-        <form />
+        <form className="relative w-full">
+          <Input
+            className="bg-navy-700 h-11 pr-24 w-full"
+            placeholder="Leave a comment..."
+          />
+          <button
+            type="submit"
+            className="flex items-center gap-2 text-indigo-400 absolute right-3 top-1/2 -translate-y-1/2 text-xs hover:text-indigo-300 cursor-pointer disabled:opacity-50"
+          >
+            Publish
+            <MessageCirclePlusIcon className="size-3" />
+          </button>
+        </form>
 
         <div className="mt-3">
           <Suspense fallback={<IssueCommentListSkeleton />}>
